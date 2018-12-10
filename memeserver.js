ffb6de49cc4b38e24893fd13c800c13322d81f2a
Noe <noemotamedi@students.berkeley.net>
2018-12-10 13:19:23 -0800

    new commit

--- memeserver.js
+++ memeserver.js
@@ -1,1 +1,52 @@
 console.log('hi')
+// call the packages we need
+var express    = require('express')      // call express
+var bodyParser = require('body-parser')
+var app        = express()     // define our app using express
+var rmongoose = require('mongoose');
+const MongoClient = require('mongodb').MongoClient
+const assert = require('assert');
+mongoose.connect('memeapp');
+var User=require('./models/user');
+
+// configure app to use bodyParser() and ejs
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine','ejs');
+
+// get an instance of the express Router
+var router = express.Router();
+
+
+// a “get” at the root of our web app: http://localhost:3000/api
+router.get('/', function(req, res) {
+
+  db.collection('cats').find({}).toArray((err, result) => {
+    if(err) {console.log(err)}
+    res.render('memeindex.ejs', {cats: result})
+  })
+});
+
+router.post('/newPost', function(req, res) {
+  console.log("POST!");  //logs to terminal
+  var fName = req.body.firstName
+  var lName = req.body.lastName
+  db.collection('cats').insertOne({fName:fName, lName:lName})
+  res.render('memeindex.ejs');  //renders index page in browser
+});
+
+
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+// START THE SERVER
+//==========================================================
+var db
+MongoClient.connect('mongodb://malbinson:berkeley@ds119436.mlab.com:19436/tester',{ useNewUrlParser: true }, (err, client) => {
+    if(err) console.log(err)
+    console.log("Connected successfully to server");
+
+    db = client.db('tester');
+    app.listen(3000, () => {
+        console.log('meow')
+    })
+})
---
+++ models/user.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var blogSchema = new Schema({
+  title:  String,
+  author: String,
+  body:   String,
+  comments: [{ body: String, date: Date }],
+  date: { type: Date, default: Date.now },
+  hidden: Boolean,
+  meta: {
+    votes: Number,
+    favs:  Number
+  }
+});
+
+//Schema → Model
+var User = mongoose.model('User', userSchema);
+
+// make this available to server.js
+module.exports = User;
